e2787b41991a7baf30aac930bff8961d165298da
Devin Ho <djxho36@gmail.com>
2022-04-19 17:17:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+//const input = document.getElementById('input');
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.querySelector('form').addEventListener('submit', function (event){
+    event.preventDefault();
+    console.log(event.target["new-task-description"]);
+  });
 });
+
+
+
